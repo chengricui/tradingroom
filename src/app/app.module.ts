3c04c15e63a1160aa41463ed6f4dc2df4d790c97
chengricui <chengri1105@gmail.com>
2018-12-13 18:55:54 +0800

    chat page building startup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,38 +3,71 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
+import { YoutubePage } from '../pages/youtube/youtube';
+import { SignalsPage} from "../pages/signals/signals";
+import { ChatPage } from '../pages/chat/chat';
+import { UsersPage} from "../pages/users/users";
+import { SettingPage} from "../pages/setting/setting";
+import { PodcastPage} from "../pages/podcast/podcast";
+import { DocumentPage} from "../pages/document/document";
+import { Mt4Page} from "../pages/mt4/mt4";
 import { TabsPage } from '../pages/tabs/tabs';
+import {HomePage} from "../pages/home/home";
+
+import { ChatPageModule} from "../pages/chat/chat.module";
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { EmojiProvider } from '../providers/emoji';
+import { ImagesProvider } from '../providers/images/images';
+import { PreloaderProvider } from '../providers/preloader/preloader';
+import { SocketsProvider } from '../providers/sockets/sockets';
+import {config} from "../configurations/configurations";
+import {SocketIoModule} from "ng-socket-io";
+import {HttpClientModule} from "@angular/common/http";
+
 
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
+    YoutubePage,
+    SignalsPage,
+    UsersPage,
+    SettingPage,
+    PodcastPage,
+    DocumentPage,
+    Mt4Page,
+    TabsPage,
+    HomePage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    HttpClientModule,
+    SocketIoModule.forRoot(config.io),
+    ChatPageModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
+    YoutubePage,
+    SignalsPage,
+    UsersPage,
+    SettingPage,
+    PodcastPage,
+    DocumentPage,
+    Mt4Page,
+    TabsPage,
+    HomePage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    EmojiProvider,
+    ImagesProvider,
+    PreloaderProvider,
+    SocketsProvider
   ]
 })
 export class AppModule {}
--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,19 +1,30 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
 
-import { AboutPage } from '../about/about';
-import { ContactPage } from '../contact/contact';
-import { HomePage } from '../home/home';
+import { YoutubePage} from "../youtube/youtube";
+import { SignalsPage} from "../signals/signals";
+import {ChatPage} from "../chat/chat";
+import { UsersPage} from "../users/users";
+import { SettingPage} from "../setting/setting";
+import { PodcastPage} from "../podcast/podcast";
+import { DocumentPage} from "../document/document";
+import {Mt4Page} from "../mt4/mt4";
 
 @Component({
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
 
-  tab1Root = HomePage;
-  tab2Root = AboutPage;
-  tab3Root = ContactPage;
+  youtubeRoot = YoutubePage;
+  signalsRoot = SignalsPage;
+  chatRoot = ChatPage;
+  usersRoot = UsersPage;
+  settingRoot = SettingPage;
+  podcastRoot = PodcastPage;
+  documentRoot = DocumentPage;
+  mt4Root = Mt4Page;
 
   constructor() {
 
   }
+
 }
